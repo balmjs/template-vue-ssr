067a172fe8997008f021c063450d440db36373ad
Elf-mousE <ifmouz@gmail.com>
2019-04-17 11:56:37 +0800

    update balm@1.3.2

--- templates/app/scripts/main.js
+++ templates/app/scripts/main.js
@@ -1,16 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
-import { createRouter } from './router';
-import { createStore } from './store';
+import App from '@/views/layouts/app';
+import { createRouter } from '@/routes';
+import { createStore } from '@/store';
 import { sync } from 'vuex-router-sync';
-
-const isProd = process.env.NODE_ENV === 'production';
+import { isProd } from '@/config';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
-// console.log(createRouter())
 /* eslint-disable */
 export function createApp() {
   // 创建 router 实例
